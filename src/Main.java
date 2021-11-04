8dfaed4900cda894a8114f9e624e2bc63a2b54f0
MatheusLMallmann <matheusl.mallmann@gmail.com>
2021-11-04 17:30:42 -0300

    Adicionando menus para obter escolha do usuário

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import Menus.ObterEscolha;
+
 public class Main {
     public static void main(String[] args){
-        
+
+        ObterEscolha obterEscolha = new ObterEscolha();
+        int escolhaDoTipoDosValores = obterEscolha.ColetaEscolha();
+        int escolhaDoFormatoDeInsercaoDosValores = obterEscolha.FormatoDeInsercaoDosDados();
+
+        switch (escolhaDoFormatoDeInsercaoDosValores){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
     }
 }
---
+++ src/Menus/ObterEscolha.java
@@ -0,0 +1,47 @@
+package Menus;
+
+import java.util.Scanner;
+
+public class ObterEscolha {
+
+    private String ColetaNome(){
+        System.out.println("Nos informe o seu nome: ");
+        Scanner input = new Scanner(System.in);
+        String nome = input.nextLine();
+
+        return nome;
+    }
+
+    public int ColetaEscolha(){
+        String nome = ColetaNome();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Olá " + nome + ", escolha uma das opções abaixo: ");
+        System.out.println("(1) - Ordenar valores inteiros;");
+        System.out.println("(2) - Ordenar valores reais;");
+        System.out.println("(3) - Ordenar valores datas;");
+        System.out.println("(4) - Ordenar palavras;");
+        System.out.println("(0) - Sair");
+
+        int escolha = Integer.parseInt(input.nextLine());
+
+        if(escolha == 0){
+            System.out.println("Encerrando sessão... Até logo :)");
+            System.exit(0);
+        }
+
+        return escolha;
+    }
+
+    public int FormatoDeInsercaoDosDados(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Escolha a forma da leitura dos dados conforme as opções abaixo: ");
+        System.out.println("(1) - Inserção manual;");
+        System.out.println("(2) - Inserção aleatórica;");
+        System.out.println("(3) - Inserção através da leitura de um arquivo;");
+        int escolha = Integer.parseInt(input.nextLine());
+
+        return escolha;
+    }
+}
