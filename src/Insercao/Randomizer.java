1cef3ee4c608f0809df05343a0d52c1e62d17e8b
MatheusLMallmann <matheusl.mallmann@gmail.com>
2021-11-05 20:25:06 -0300

    Adicionando inserção randomica de valores inteiros, reais, strings e datas

---
+++ src/Insercao/Randomizer.java
@@ -0,0 +1,69 @@
+package Insercao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+public class Randomizer {
+
+    Random random = new Random();
+
+    public List<Integer> geradorListaInteiros(){
+        List<Integer> listaInteiros = new ArrayList<>();
+        int max = 1000, min = 0, numero = 0;
+        for(int i = 0; i < 100; i++){
+            numero = random.nextInt((max - min) + 1) + min;
+            listaInteiros.add(i, numero);
+        }
+        return listaInteiros;
+    }
+
+    public List<Double> geradorListaFloat(){
+        List<Double> listaFloat = new ArrayList<>();
+        Double max = 1000.0, min = 0.0;
+        for(int i = 0; i < 100; i++){
+            Double numero = min + (max - min) * random.nextDouble();
+            listaFloat.add(i, numero);
+        }
+        return listaFloat;
+    }
+
+    public List<String> geradorListaString(){
+        List<String> listaString = new ArrayList<>();
+        String palavra = "";
+
+        for(int j = 0; j < 100; j++){
+            for(int i = 0; i < 7; i++){
+                int rnd = (int) (Math.random() * 52); // or use Random or whatever
+                char base = (rnd < 26) ? 'A' : 'a';
+                palavra += (char) (base + rnd % 26);
+            }
+            listaString.add(j, palavra);
+            palavra = "";
+        }
+        return listaString;
+    }
+
+    public List<Date> geradorListaDatas() throws ParseException {
+        List<Date> listaDatas = new ArrayList<>();
+        Date dataGerada = null;
+        String dataString = "";
+        int diaMin = 1, diaMax = 31;
+        int mesMin = 1, mesMax = 12;
+
+        for (int i = 0; i < 100; i++){
+            int dia = random.nextInt((diaMax - diaMin) + 1) + diaMin;
+            int mes = random.nextInt((mesMax - mesMin) + 1) + mesMin;
+
+            dataString = dia + "/" + mes + "/" + "2021";
+            dataGerada = new SimpleDateFormat("dd/MM/yyyy").parse(dataString);
+
+            listaDatas.add(i, dataGerada);
+            dataString = " ";
+        }
+        return listaDatas;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Insercao.Randomizer;
 import Inserção.Manual;
 import Menus.ObterEscolha;
 import Ordenacao.BubbleSort;
@@ -12,68 +13,71 @@ public class Main {
 
     public static void main(String[] args) throws ParseException {
 
-        ObterEscolha obterEscolha = new ObterEscolha();
-        Manual manual = new Manual();
-        BubbleSort bs = new BubbleSort();
-        InsertionSort is = new InsertionSort();
-        SelectionSort ss = new SelectionSort();
-        int escolhaDoTipoDosValores = obterEscolha.ColetaEscolha();
-        int escolhaDoFormatoDeInsercaoDosValores = obterEscolha.FormatoDeInsercaoDosDados();
+        Randomizer randomizer = new Randomizer();
+        randomizer.geradorListaDatas();
 
-        switch (escolhaDoFormatoDeInsercaoDosValores){
-            case 1:
-                if (escolhaDoTipoDosValores == 1){
-                    List<Integer> lista = manual.manualInteger();
-                    System.out.println("Vetor original:");
-                    System.out.println(lista);
-                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
-                    bs.ordenacaoInteger(lista);
-                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
-                    is.ordenacaoInteger(lista);
-                    System.out.println("Vetor ordenado utilizando Selection Sort");
-                    ss.ordenacaoInteger(lista);
-                }
-
-                if (escolhaDoTipoDosValores == 2){
-                    List<Double> lista = manual.manualFloat();
-                    System.out.println("Vetor original:");
-                    System.out.println(lista);
-                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
-                    bs.ordenacaoFloat(lista);
-                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
-                    is.ordenacaoFloat(lista);
-                    System.out.println("Vetor ordenado utilizando Selection Sort");
-                    ss.ordenacaoFloat(lista);
-                }
-
-                if (escolhaDoTipoDosValores == 3){
-                    List<Date> lista = manual.manualDate();
-                    System.out.println("Vetor original:");
-                    System.out.println(lista);
-                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
-                    bs.ordenacaoDate(lista);
-                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
-                    is.ordenacaoDate(lista);
-                    System.out.println("Vetor ordenado utilizando Selection Sort:");
-                    ss.ordenacaoData(lista);
-                }
-
-                if (escolhaDoTipoDosValores == 4){
-                    List<String> lista = manual.manualString();
-                    System.out.println("Vetor original:");
-                    System.out.println(lista);
-                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
-                    bs.ordenacaoString(lista);
-                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
-                    is.ordenacaoString(lista);
-                    System.out.println("Vetor ordenado utilizando Selection Sort");
-                    ss.ordenacaoString(lista);
-                }
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-        }
+//        ObterEscolha obterEscolha = new ObterEscolha();
+//        Manual manual = new Manual();
+//        BubbleSort bs = new BubbleSort();
+//        InsertionSort is = new InsertionSort();
+//        SelectionSort ss = new SelectionSort();
+//        int escolhaDoTipoDosValores = obterEscolha.ColetaEscolha();
+//        int escolhaDoFormatoDeInsercaoDosValores = obterEscolha.FormatoDeInsercaoDosDados();
+//
+//        switch (escolhaDoFormatoDeInsercaoDosValores){
+//            case 1:
+//                if (escolhaDoTipoDosValores == 1){
+//                    List<Integer> lista = manual.manualInteger();
+//                    System.out.println("Vetor original:");
+//                    System.out.println(lista);
+//                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
+//                    bs.ordenacaoInteger(lista);
+//                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
+//                    is.ordenacaoInteger(lista);
+//                    System.out.println("Vetor ordenado utilizando Selection Sort");
+//                    ss.ordenacaoInteger(lista);
+//                }
+//
+//                if (escolhaDoTipoDosValores == 2){
+//                    List<Double> lista = manual.manualFloat();
+//                    System.out.println("Vetor original:");
+//                    System.out.println(lista);
+//                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
+//                    bs.ordenacaoFloat(lista);
+//                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
+//                    is.ordenacaoFloat(lista);
+//                    System.out.println("Vetor ordenado utilizando Selection Sort");
+//                    ss.ordenacaoFloat(lista);
+//                }
+//
+//                if (escolhaDoTipoDosValores == 3){
+//                    List<Date> lista = manual.manualDate();
+//                    System.out.println("Vetor original:");
+//                    System.out.println(lista);
+//                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
+//                    bs.ordenacaoDate(lista);
+//                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
+//                    is.ordenacaoDate(lista);
+//                    System.out.println("Vetor ordenado utilizando Selection Sort:");
+//                    ss.ordenacaoData(lista);
+//                }
+//
+//                if (escolhaDoTipoDosValores == 4){
+//                    List<String> lista = manual.manualString();
+//                    System.out.println("Vetor original:");
+//                    System.out.println(lista);
+//                    System.out.println("Vetor ordenado utilizando Bubble Sort:");
+//                    bs.ordenacaoString(lista);
+//                    System.out.println("Vetor ordenado utilizando Insertion Sort:");
+//                    is.ordenacaoString(lista);
+//                    System.out.println("Vetor ordenado utilizando Selection Sort");
+//                    ss.ordenacaoString(lista);
+//                }
+//                break;
+//            case 2:
+//                break;
+//            case 3:
+//                break;
+//        }
     }
 }
